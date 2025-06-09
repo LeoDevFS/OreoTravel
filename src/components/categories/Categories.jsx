91b8988bd25de20265816d11096d81d3063ba591
Leo <leonelsicaro@gmail.com>
2025-06-09 23:58:38 +0200

    home & categories

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import SectionTitle from "../title/SectionTitle";
+import { RiArrowLeftDoubleFill, RiArrowRightDoubleFill } from "react-icons/ri";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "./categories.css";
+import { categories } from "../../Data";
 
 function Categories() {
   return (
-    <div>Categories</div>
-  )
+    <section className="categories container section">
+      <div className="category-header">
+        <SectionTitle
+          title="Categories"
+          decription="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam laborum facilis mollitia reiciendis explicabo perferendis laudantium, dolores blanditiis accusamus similique illo odit exercitationem neque id. Voluptatem provident cumque dolorem quam."
+        />
+        <div className="swiper-btns">
+          <button className="swiper-btn prev-btn">
+            <RiArrowLeftDoubleFill />
+          </button>
+
+          <button className="swiper-btn next-btn">
+            <RiArrowRightDoubleFill />
+          </button>
+        </div>
+      </div>
+
+      <Swiper
+      slidesPerView={1}
+      spaceBetween={12}
+      grabCursor={true}
+      loop={true}
+      navigation={{
+        nextEl: '.next-btn',
+        prevEl:'.prev-btn'
+      }}
+        
+        modules={[Navigation]}
+        breakpoints={{
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 5,
+          },
+          1208: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+        }}
+        className="mySwiper"
+      >
+        {categories.map(({ img, title }, index) => {
+          return (
+            <SwiperSlide className="category-slide" key={index}>
+              <img src={img} alt="" className="category-img" />
+              <h3 className="category-title">{title}</h3>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 }
 
-export default Categories
+export default Categories;
---
+++ src/components/title/SectionTitle.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./sectionTitle.css"
+
+function SectionTitle({title,description}) {
+  return (
+    <div className="section-header">
+        <h2 className="section-title"> Categories
+        </h2>
+            <p className="section-description"> 
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur ex labore asperiores adipisci consequuntur quia temporibus ipsam quae enim quam?
+            </p>
+    </div>
+  )
+}
+
+export default SectionTitle
